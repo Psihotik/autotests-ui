9a700236f32fd201823841dc49aed80fa02f0ec2
Psihotik <Psihotik999@gmail.com>
2026-09-23 10:36:10 +0300

    Знакомство с PageComponent

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,3 +1,4 @@
+from components.navigation.navbar_component import NavbarComponent
 from pages.base_pages import BasePage
 from playwright.sync_api import Page, expect
 
@@ -5,6 +6,9 @@ from playwright.sync_api import Page, expect
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.navbar = NavbarComponent(page)
+        
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
         self.students_title = page.get_by_test_id('students-widget-title-text')
